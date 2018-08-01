a101eec048678ea8e54045996af8228e37b6cc99
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2018-08-01 19:10:53 +0200

    Remove yarn workspace

--- packages/rauks-player/rn-cli.config.js
+++
@@ -1,4 +0,0 @@
-const getConfig = require('metro-bundler-config-yarn-workspaces');
-const path = require('path');
-const options = { nodeModules: path.resolve(__dirname, '..', '..') }
-module.exports = getConfig(__dirname, options);
