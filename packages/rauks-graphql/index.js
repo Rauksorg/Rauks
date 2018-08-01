a51095aab48e4286d0c61a9d54cebc621bc78b58
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2018-08-01 22:50:15 +0200

    Setup react-native with yarn workspace

---
+++ packages/rauks-graphql/index.js
@@ -0,0 +1,35 @@
+const { ApolloServer, gql, MockList } = require('apollo-server')
+const casual = require('casual')
+
+const typeDefs = gql`
+type Book {
+  title: String
+  author: Author
+}
+type Query {
+  getBooks: [Book]
+  getAuthors: [Author]
+}
+type Author {
+  name: String
+  books: [Book]
+}
+`
+const mocks = {
+  Book: () => ({
+    title: casual.title,
+  }),
+  Author: () => ({
+    name: casual.name,
+    books: () => new MockList([2, 6]),
+  }),
+  Query: () => ({
+    getAuthors: () => new MockList([2, 6]),
+    getBooks: () => new MockList([2, 6]),
+  }),
+}
+const server = new ApolloServer({ typeDefs, mocks })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
