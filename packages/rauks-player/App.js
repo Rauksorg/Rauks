536318554edd608add5101a47f19854145a5baea
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2018-08-02 13:07:05 +0200

    connect apollo-boust to rauks-graphql

--- packages/rauks-player/App.js
+++ packages/rauks-player/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/"
+});
+
+import { ApolloProvider } from "react-apollo";
+
 export default class App extends React.Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
+      </ApolloProvider>
     );
   }
 }
