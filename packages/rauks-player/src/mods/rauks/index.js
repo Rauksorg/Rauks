15ffe5932a6498e156da81997714e89d59b31f81
Thibaut Constant <thibaut.constant@isisphinx.com>
2018-08-17 22:52:14 +0200

    move mods from server to client

---
+++ packages/rauks-player/src/mods/rauks/index.js
@@ -0,0 +1,13 @@
+import character from './characters'
+import rules from './rules'
+import dice from './dices'
+import ressources from './ressources'
+import states from './states'
+import themes from './themes'
+
+const infos = {
+  name: 'My mod name',
+  description: 'The description of the game',
+  system: 'The system it is compatible with',
+  language: 'The targeted language eg : fr-FR'
+}
---
+++ packages/rauks-player/src/mods/rauks/modules/dices.js
@@ -0,0 +1,20 @@
+const dices = [dice1]
+
+const dice1 = {
+  name: String,
+  description: String,
+  image: String,
+  backgroundColor: String,
+  color: String,
+  faces: [face1],
+  min: Int,
+  max: Int
+}
+
+const face1 = {
+  name: String,
+  description: String,
+  image: String
+}
+
+export default dices
