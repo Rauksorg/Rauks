0141c4701302c80043907da9f233250753efd700
Quentin Constant <quentin@constant.eu>
2018-08-25 22:04:25 +0200

    Création des dés Bleu/Orange/Rouge avec les faces correspondantes

--- packages/rauks-player/src/mods/rauks/modules/dices.js
+++ packages/rauks-player/src/mods/rauks/modules/dices.js
@@ -1,33 +1,69 @@
 const faces = {
-  echec: {
-    name: 'echec',
-    description: '',
-    image: ''
-  },
-  sucess: {
-    name: 'sucess',
-    description: '',
-    image: ''
-  }
+  const faces = [
+    {
+      name: 'réussite critique',
+      description: `Réussite de l'action, et le joueur propose un fait supplémentaire au MJ`,
+      image: '/'
+    },
+    {
+      name: 'échec simple',
+      description: `Echec de l'action`,
+      image: '/'
+    }
+    {
+      name: 'réussite compétence',
+      description: `Réussite de l'action si le personnage peut justifier d'une compétence lors du jet ou éventuellement d'un trait ou d'une circonstance`,
+      image: '/'
+    }
+    {
+      name: 'échec critique',
+      description: `Echec de l'action, en cas de relance l'échec deviendra critique ne sera plus relançable et provoquera des conséquences supplémentaires.`,
+      image: '/'
+    }
+    {
+      name: '2',
+      description: `Réussite de l'action si le personnage dispose de 2 ou plus dans sa caractéristiques`,
+      image: '/'
+    },
+    {
+      name: '3',
+      description: `Réussite de l'action si le personnage dispose de 3 ou plus dans sa caractéristiques`,
+      image: '/'
+    },
+    {
+      name: '4',
+      description: `Réussite de l'action si le personnage dispose de 4 ou plus dans sa caractéristiques`,
+      image: '/'
+    },
 }
 
 const blueDice = {
   name: 'Dés bleu',
-  description: '',
+  description: 'Dés correspondant aux actions relativement simples',
   image: '',
   backgroundColor: '#2b78e4',
   color: '#ffffff',
-  faces: [faces.echec, faces.sucess],
+  faces: [faces.réussite critique,faces.réussite compétence,faces.4, faces.3,faces.2,faces.échec critique],
+  min: 1,
+  max: 1
+}
+const orangeDice = {
+  name: 'Dés orange',
+  description: 'Dés correspondant aux actions difficiles',
+  image: '',
+  backgroundColor: '#2b78e4',
+  color: '#ffffff',
+  faces: [faces.réussite critique,faces.réussite compétence,faces.4, faces.3,faces.échec simple,faces.échec critique],
   min: 1,
   max: 1
 }
 const redDice = {
   name: 'Dés rouge',
-  description: '',
+  description: 'Dés correspondant aux actions les plus difficiles et risquées',
   image: '',
   backgroundColor: '#cf2a27',
   color: '#ffffff',
-  faces: [faces.echec, faces.sucess],
+  faces: [faces.réussite critique,faces.réussite compétence,faces.4, faces.échec simple,faces.échec critique,faces.échec critique],
   min: 1,
   max: 1
 }
