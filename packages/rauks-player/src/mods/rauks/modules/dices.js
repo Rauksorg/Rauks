62a27b4fe543aa09a81300f234a03f953fcf6e72
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2018-08-25 23:04:10 +0200

    partial correction for dices

--- packages/rauks-player/src/mods/rauks/modules/dices.js
+++ packages/rauks-player/src/mods/rauks/modules/dices.js
@@ -1,36 +1,35 @@
 const faces = {
-  const faces = [
-    {
+    epicSuccess:{
       name: 'réussite critique',
       description: `Réussite de l'action, et le joueur propose un fait supplémentaire au MJ`,
       image: '/'
     },
-    {
+    fail:{
       name: 'échec simple',
       description: `Echec de l'action`,
       image: '/'
-    }
-    {
+    },
+    skill:{
       name: 'réussite compétence',
       description: `Réussite de l'action si le personnage peut justifier d'une compétence lors du jet ou éventuellement d'un trait ou d'une circonstance`,
       image: '/'
-    }
-    {
+    },
+    epicFail:{
       name: 'échec critique',
-      description: `Echec de l'action, en cas de relance l'échec deviendra critique ne sera plus relançable et provoquera des conséquences supplémentaires.`,
+      description: `Echec de l'action, en cas de relance l'échec deviendra critique ne sera plus relançable et provoquera des conséquences supplémentaires`,
       image: '/'
-    }
-    {
+    },
+    two:{
       name: '2',
       description: `Réussite de l'action si le personnage dispose de 2 ou plus dans sa caractéristiques`,
       image: '/'
     },
-    {
+    three:{
       name: '3',
       description: `Réussite de l'action si le personnage dispose de 3 ou plus dans sa caractéristiques`,
       image: '/'
     },
-    {
+    four:{
       name: '4',
       description: `Réussite de l'action si le personnage dispose de 4 ou plus dans sa caractéristiques`,
       image: '/'
@@ -43,7 +42,7 @@ const blueDice = {
   image: '',
   backgroundColor: '#2b78e4',
   color: '#ffffff',
-  faces: [faces.réussite critique,faces.réussite compétence,faces.4, faces.3,faces.2,faces.échec critique],
+  faces: [faces.success,faces. critique,faces.réussite compétence,faces.4, faces.3,faces.2,faces.échec critique],
   min: 1,
   max: 1
 }
@@ -53,7 +52,7 @@ const orangeDice = {
   image: '',
   backgroundColor: '#2b78e4',
   color: '#ffffff',
-  faces: [faces.réussite critique,faces.réussite compétence,faces.4, faces.3,faces.échec simple,faces.échec critique],
+  faces: [faces.réussite, faces.epicSuccess,faces.réussite, faces.skill,faces.four, faces.three,faces.fail,faces.epicFail],
   min: 1,
   max: 1
 }
@@ -68,5 +67,5 @@ const redDice = {
   max: 1
 }
 
-const dices = [blueDice, redDice]
+const dices = [blueDice,orangeDice, redDice]
 export default dices
