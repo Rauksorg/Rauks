53cf5169035051d0b46306497bfd579e0854a165
Quentin Constant <quentin@constant.eu>
2018-08-29 14:06:40 +0200

    Correction mineur sur les dés

--- packages/rauks-player/src/mods/rauks/modules/dices.js
+++ packages/rauks-player/src/mods/rauks/modules/dices.js
@@ -1,127 +1,91 @@
-import epicFailImg from './dices/efail.svg'
-import fourImg from './dices/four.svg'
-import skillImg from './dices/skill.svg'
-import successImg from './dices/success.svg'
-import threeImg from './dices/three.svg'
-import twoImg from './dices/two.svg'
-import failImg from './dices/fail.svg'
-import doubleSuccessImg from './dices/dsuccess.svg'
-import blueDiceImg from './dices/3Dblue.svg'
-import orangeDiceImg from './dices/3Dorange.svg'
-import redDiceImg from './dices/3Dred.svg'
-import darkDiceImg from './dices/3Ddark.svg'
-
-
-
 const faces = {
-  epicSuccess: {
-    name: 'réussite critique',
-    description: `Réussite de l'action, et le joueur propose un fait supplémentaire au MJ`,
-    image: successImg
-  },
-  fail: {
-    name: 'échec simple',
-    description: `Echec de l'action`,
-    image: failImg
-  },
-  skill: {
-    name: 'réussite compétence',
-    description: `Réussite de l'action si le personnage peut justifier d'une compétence lors du jet ou éventuellement d'un trait ou d'une circonstance`,
-    image: skillImg
-  },
-  epicFail: {
-    name: 'échec critique',
-    description: `Echec de l'action, en cas de relance l'échec deviendra critique ne sera plus relançable et provoquera des conséquences supplémentaires`,
-    image: epicFailImg
-  },
-  two: {
-    name: '2',
-    description: `Réussite de l'action si le personnage dispose de 2 ou plus dans sa caractéristiques`,
-    image: twoImg
-  },
-  three: {
-    name: '3',
-    description: `Réussite de l'action si le personnage dispose de 3 ou plus dans sa caractéristiques`,
-    image: threeImg
-  },
-  four: {
-    name: '4',
-    description: `Réussite de l'action si le personnage dispose de 4 ou plus dans sa caractéristiques`,
-    image: fourImg
-  },
-  damage: {
-    name: '!',
-    description: `Le dé occasionne un point de dégâts`,
-    image: doubleSuccessImg
-  },
-  resist: {
-    name: 'x',
-    description: `Le dé n'occasionne pas de dégâts`,
-    image: successImg
-  }
+    epicSuccess:{
+      name: 'réussite critique',
+      description: `Réussite de l'action, et le joueur propose un fait supplémentaire au MJ`,
+      image: '/'
+    },
+    fail:{
+      name: 'échec simple',
+      description: `Echec de l'action`,
+      image: '/'
+    },
+    skill:{
+      name: 'réussite compétence',
+      description: `Réussite de l'action si le personnage peut justifier d'une compétence lors du jet ou éventuellement d'un trait ou d'une circonstance`,
+      image: '/'
+    },
+    epicFail:{
+      name: 'échec critique',
+      description: `Echec de l'action, en cas de relance l'échec deviendra critique ne sera plus relançable et provoquera des conséquences supplémentaires`,
+      image: '/'
+    },
+    two:{
+      name: '2',
+      description: `Réussite de l'action si le personnage dispose de 2 ou plus dans sa caractéristiques`,
+      image: '/'
+    },
+    three:{
+      name: '3',
+      description: `Réussite de l'action si le personnage dispose de 3 ou plus dans sa caractéristiques`,
+      image: '/'
+    },
+    four:{
+      name: '4',
+      description: `Réussite de l'action si le personnage dispose de 4 ou plus dans sa caractéristiques`,
+      image: '/'
+    },
+    damage:{
+      name: '!',
+      description: `Le dé occasionne un point de dégâts`,
+      image: '/'
+    },
+    resist:{
+      name: 'x',
+      description: `Le dé n'occasionne pas de dégâts`,
+      image: '/'
+    }
 }
 
 const blueDice = {
   name: 'Dés bleu',
   description: 'Dés correspondant aux actions classiques',
-  image: blueDiceImg,
+  image: '',
   backgroundColor: '#2b78e4',
   color: '#ffffff',
-  faces: [
-    faces.epicSuccess,
-    faces.skill,
-    faces.four,
-    faces.three,
-    faces.two,
-    faces.epicFail
-  ],
+  faces: [faces.epicSuccess,faces.skill,faces.four, faces.three,faces.two,faces.epicFail],
   min: 1,
   max: 1
 }
 const orangeDice = {
   name: 'Dés orange',
   description: 'Dés correspondant aux actions difficiles',
-  image: orangeDiceImg,
-  backgroundColor: '#ff9900',
+  image: '',
+  backgroundColor: '#2b78e4',
   color: '#ffffff',
-  faces: [
-    faces.epicSuccess,
-    faces.skill,
-    faces.four,
-    faces.three,
-    faces.fail,
-    faces.epicFail
-  ],
+  faces: [faces.epicSuccess,faces.skill,faces.four, faces.three,faces.fail,faces.epicFail],
   min: 1,
   max: 1
 }
 const redDice = {
   name: 'Dés rouge',
   description: 'Dés correspondant aux actions les plus difficiles et risquées',
-  image: redDiceImg,
+  image: '',
   backgroundColor: '#cf2a27',
   color: '#ffffff',
-  faces: [
-    faces.epicSuccess,
-    faces.skill,
-    faces.four,
-    faces.fail,
-    faces.epicFail,
-    faces.epicFail
-  ],
+  faces: [faces.epicSuccess,faces.skill,faces.four, faces.fail,faces.epicFail,faces.epicFail],
   min: 1,
   max: 1
 }
 const damageDice = {
   name: 'Dés dé dégâts',
   description: `Dés lancés lors d'un jet de dégâts`,
-  image: darkDiceImg,
-  backgroundColor: '#333333',
+  image: '',
+  backgroundColor: '#cf2a27',
   color: '#ffffff',
-  faces: [faces.damage, faces.resist],
+  faces: [faces.damage,faces.resist],
   min: 2,
   max: 9
 }
 
-const dices = [blueDice, orangeDice, redDice, damageDice]
+const dices = [blueDice,orangeDice,redDice,damageDice]
 export default dices
